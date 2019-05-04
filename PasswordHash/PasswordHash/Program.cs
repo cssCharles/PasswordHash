8c818a8a56ba4ba7f2f35bbe12441d09fa9e08c7
Charles May <cssCharles@users.noreply.github.com>
2019-05-04 10:33:58 -0400

    Add files via upload

---
+++ PasswordHash/PasswordHash/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Security.Cryptography;
+namespace PasswordHash
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			bool Invalid = true;
+			string theHash = PasswordHash.CreateHash("ChaRLi3IzG0D");
+			Console.WriteLine(theHash);
+			while (Invalid)
+			{
+				
+				Console.Clear();
+				Console.WriteLine("Enter Password");
+				string PW = Console.ReadLine();
+
+				if (PasswordHash.ValidatePassword(PW, theHash))
+				{
+					Console.WriteLine("Welcome");
+					Invalid = false;
+				}
+				else
+				{
+					Console.WriteLine("Invalid Password");
+			Console.ReadLine();
+				}
+			}
+
+			Console.ReadLine();
+		}
+
+
+	}
+
+}
